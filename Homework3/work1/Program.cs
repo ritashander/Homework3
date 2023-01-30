e9fa07c8f302134ad35687741668308c02ed57c3
Rita Shander <iamshizooo@gmail.com>
2023-01-30 18:21:23 +0300

    work1 done

---
+++ Homework3/work1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = new Random().Next(1, 10000);
+int t = num;
+int rev = 0;
+int d;
+
+System.Console.WriteLine(num);
+
+while(num > 0)
+    {
+         d = num % 10;
+         rev = rev * 10 + d;
+         num = num / 10;
+    }
+    if(t == rev)
+    {
+        Console.WriteLine("It's a palindrome number");
+    }
+    else
+    {
+        Console.WriteLine("It's a not palindrome number");
+    }
+
+		 
