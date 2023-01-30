de684583a26184fc333575f2e1103c1ab7b5c450
Rita Shander <iamshizooo@gmail.com>
2023-01-30 18:35:40 +0300

    work2 done

---
+++ Homework3/work2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
+
+int x1 = new Random().Next(-10, 10);
+int y1 = new Random().Next(-10, 10);
+int z1 = new Random().Next(-10, 10);
+int x2 = new Random().Next(-10, 10);
+int y2 = new Random().Next(-10, 10);
+int z2 = new Random().Next(-10, 10);
+
+System.Console.WriteLine($"x1 = {x1}, y1 = {y1}, z1 = {z1}, y2 = {y2}, x2 = {x2}, z2 = {z2}");
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+System.Console.WriteLine(result);
