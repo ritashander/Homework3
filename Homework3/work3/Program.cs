eeb22f815d9f8c40eac746785b3d498bd755ff58
Rita Shander <iamshizooo@gmail.com>
2023-01-30 18:41:39 +0300

    work3 done

---
+++ Homework3/work3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int num = new Random().Next(1, 10);
+
+System.Console.WriteLine($"Число = {num}");
+
+for(int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
